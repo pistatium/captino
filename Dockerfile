d943e33b451bad81dce828a21bfd7cea97b6bced
Kimihiro N <kimihiro.nisimura@gmail.com>
2018-09-13 19:06:00 +0900

    :+1: Indexページを追加

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,4 @@ MAINTAINER pistatium
 
 EXPOSE 8000
 
-COPY src/main.js /app/index.js 
+COPY src /app
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const http = require('http');
 const url = require('url');
 const puppeteer = require('puppeteer');
 const exec = require('child_process').exec;
-const fileSystem = require('fs');
+const fs = require('fs');
 
 const is_valid_url = (str) => {
   const pattern = /(http|https):\/\/(\w+:{0,1}\w*)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%!\-\/]))?/;
@@ -30,9 +30,18 @@ const server = http.createServer();
 server.on('request', async (req, res) => {
     let parsed = url.parse(req.url, true);
     let params = parsed.query;
+    let pathname = parsed.pathname;
+    if (pathname === '/') {
+        fs.readFile('/app/static/index.html', (err, html) => {
+            res.writeHeader(200, {"Content-Type": "text/html"});  
+            res.write(html);  
+            res.end();  
+        });
+        return;
+    }
     if (parsed.pathname !== '/capture') {
         res.statusCode = 404;
-        return res.end('<a href="/capture?url=https://google.com">Try it.</a>');
+        return res.end('404');
     }
     if (!params.url) {
         res.statusCode = 400;
@@ -55,10 +64,11 @@ server.on('request', async (req, res) => {
         res.statusCode = 500;
         return res.end('Capture error:\n ' + err.message.split("\n")[0]);
     }
-	let stat = fileSystem.statSync(path);
+	let stat = fs.statSync(path);
 	res.writeHead(200, {'Content-Type': 'image/png', 'Content-Lenght': stat.size});
-	var readStream = fileSystem.createReadStream(path);
+	var readStream = fs.createReadStream(path);
 	readStream.pipe(res);
 });
+
 server.listen(8000, '0.0.0.0');
 console.log('Server running at http://0.0.0.0:8000/');
